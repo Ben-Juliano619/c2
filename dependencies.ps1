729d7da643938b65f198b3d538e336cdaa1fa51b
Ben-Juliano619 <111253855+Ben-Juliano619@users.noreply.github.com>
2024-03-29 11:08:53 -0400

    Add files via upload
    PowerShell dependency script

---
+++ dependencies.ps1
@@ -0,0 +1,7 @@
+git clone https://github.com/microsoft/vcpkg
+.\vcpkg\bootstrap-vcpkg.bat
+.\vcpkg install boost-uuid:x64-windows
+.\vcpkg install boost-property-tree:x64-windows
+.\vcpkg install boost-system:x64-windows
+.\vcpkg install cpr:x64-windows
+.\vcpkg install nlohmann-json:x64-windows
